d77fd254615ce6a2bd2e757d31b3518a4a26f2e7
VladislavS999 <phpmailer1999@gmail.com>
2022-08-29 17:09:19 +0300

    Javascript код вынесен в отдельный файл

--- calculator.php
+++ calculator.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once 'operations.php'; // Подключение файла с массивом символов операций 
+
 $statement = $_POST['statement'];
+
+/*echo '<pre>';
+var_dump($statement);
+echo '</pre>';*/
 
 echo json_encode(['result' => $statement]);
---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('form').submit(function(e) {
+        e.preventDefault(); // Отмена действий браузера по умолчанию
+
+        // Отправляем данные на сервер ajax-запросом
+        $.ajax({
+            type: "POST",
+            url: 'calculator.php',
+            data: $(this).serialize(),
+            success: function(response){
+                var jsonData = JSON.parse(response); // Принимаем ответ от сервера
+
+                document.getElementById('result').innerHTML = 'Результат вычисления: ' + jsonData.result;
+            }
+        });
+    });
+});
