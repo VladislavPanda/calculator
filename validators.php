23fdbb5751dcc2eaafe0b822400233816aa31552
VladislavS999 <phpmailer1999@gmail.com>
2022-08-30 19:40:40 +0300

    - Создана функция проверки корректности ввода арифметических символов - Создан массив ошибок для валидации

--- validators.php
+++ validators.php
@@ -1,35 +1,31 @@
 <?php
 
 // Функция проверки - есть ли в строке буквенные символы (латинские или кирилличиские). Если да, выбрасывается ошибка
-function validateAcceptableSymbols($statement){
+function validateAcceptableSymbols($statement, &$errors){
     $latinSymbolsFlag = preg_match("/[a-z]/i", $statement); // Проверка на латинские буквенные символы
     $cyrrilicSymbolsFlag = preg_match("/[\p{Cyrillic}]/u", $statement); // Проверка на кириллические буквенные символы
-    
-    if($latinSymbolsFlag == 1 || $cyrrilicSymbolsFlag == 1){ // Если найдены латинские или кириллические буквенные символы в строке выражения - возвращается ошибка 
-        echo json_encode(['result' => 'Ошибка! В строке содержатся недопустимые символы (латиница или кириллица)'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-        exit;
-    }
+
+    if($latinSymbolsFlag == 1) $errors[] = 'Ошибка! В строке содержатся недопустимые символы (латиница)';
+    if($cyrrilicSymbolsFlag == 1) $errors[] = 'Ошибка! В строке содержатся недопустимые символы (кириллица)';
 }
 
 // Функция проверки присутствия арифметических символов в строке выражения
-function validateArithmeticSymbolsExistance($statement, $operations){
+function validateArithmeticSymbolsExistance($statement, $operations, &$errors){
     $flag = false; // Стартовое значение флага
 
     for($i = 0; $i < sizeof($operations); $i++) if(strpos($statement, $operations[$i]) !== false) $flag = true; // Если в строке найден арифметический символ, значение флага меняется на истину
-    if($flag === false){ // Если нет арифметических символов - возращается ошибка
-        echo json_encode(['result' => 'Ошибка! В строке отсутствуют арифметические символы'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-        exit;
-    }
+    if($flag === false) $errors[] = 'Ошибка! В строке отсутствуют арифметические символы'; // Если нет арифметических символов - возращается ошибка
 }
 
 // Функция проверки строки выражения: следуют ли арифметические символы друг за другом
-function validateSequentialOperators($statement, $operations){
+function validateSequentialOperators($statement, $operations, &$errors){
     $statement = str_split($statement);
 
+    if(in_array($statement[0], $operations) || in_array(end($statement), $operations))
+        $errors[] = 'Ошибка! Арифметические символы не могут располагаться в начале или конце строки';
+
     for($i = 0; $i < sizeof($statement); $i++){
-        if(in_array($statement[$i], $operations) && in_array($statement[$i+1], $operations)){
-            echo json_encode(['result' => 'Ошибка! Обнаружен повтор арифметических символов'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-            exit;
-        }
+        if(in_array($statement[$i], $operations) && in_array($statement[$i+1], $operations))
+            $errors[] = 'Ошибка! Обнаружен повтор арифметических символов';
     }
 }
